c77b7a48695610b47e1bb57ce5281e55ca8e59e1
TanPhong <TanPhong@LAPTOP-VR5FEEV8>
2023-05-11 13:00:06 +0700

    first commit

---
+++ src/main/java/com/Ngay3/Ngay3/Ngay3Application.java
@@ -0,0 +1,13 @@
+package com.Ngay3.Ngay3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ngay3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ngay3Application.class, args);
+	}
+
+}
